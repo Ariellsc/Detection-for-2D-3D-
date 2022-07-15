576015aad79a9ff9ca1c097128c87bac8530a8fb
liushichao <arieljy@163.com>
2022-07-15 17:53:45 +0800

    vgg

--- VGGNet/model.py
+++ VGGNet/model.py
@@ -1,9 +1,8 @@
-import torch
 import torch.nn as nn
-
+import torch
 
 # official pretrain weights
-model_url = {
+model_urls = {
     'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
     'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
     'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
@@ -12,15 +11,15 @@ model_url = {
 
 
 class VGG(nn.Module):
-    def __int__(self, features, num_classes=1000, init_weights=False):
-        super(VGG, self).__int__()
+    def __init__(self, features, num_classes=1000, init_weights=False):
+        super(VGG, self).__init__()
         self.features = features
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=512*7*7, out_features=4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),  # 以50%的比例随机失活神经元
             nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
+            nn.ReLU(True),
             nn.Dropout(p=0.5),
             nn.Linear(4096, num_classes)
         )
@@ -28,11 +27,13 @@ class VGG(nn.Module):
             self._initialize_weights()
 
     def forward(self, x):
-        x = self.features(x)  # N*3*24*24
-        x = torch.flatten(x, start_dim=1)  # N*512*7*7
-        x = self.classifier(x)  # N x 512*7*7
+        # N x 3 x 224 x 224
+        x = self.features(x)
+        # N x 512 x 7 x 7
+        x = torch.flatten(x, start_dim=1)  # 从第1维度开始进行展平操作，第0维度为batch。
+        # N x 512*7*7
+        x = self.classifier(x)
         return x
-
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -45,3 +46,36 @@ class VGG(nn.Module):
                 nn.init.xavier_uniform_(m.weight)
                 # nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
+
+
+# 构建网络
+def make_features(cfg: list):
+    layers = []  # 空列表layers用于存放每一层结构
+    in_channels = 3  # RGB彩色图片，输入通道3
+    for v in cfg:
+        # if Maxpool
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(True)]
+            in_channels = v  # set v as new input of next network layer
+    return nn.Sequential(*layers)  # 通过nn.Sequential函数将列表layers以非关键字参数的形式传入（*layer：以非关键字参数传入）,详见nn.Sequential
+
+
+# 'M'表示Maxpool
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+# 实例化vgg network
+def vgg(model_name="vgg16", **kwargs):  # **kwargs：接受可变长度的关键字参数字典作为实参传入函数
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+
+    model = VGG(make_features(cfg), **kwargs)
+    return model
